cc35bcf1e6a4ea002cebdf198043210ac72e70cd
Ole Jorgensen <ole@scripler.com>
2013-06-21 10:41:08 +0200

    changes to frontpage javascript that controls color shift

--- public/js/pages/frontpage.jquery.js
+++ public/js/pages/frontpage.jquery.js
@@ -52,6 +52,113 @@ $(document).ready(function(){
 	    $('.menu-contact').removeClass('active');
 	});
 
+    var scroll_pos = 0;
+    var animation_begin_pos = 0; //where you want the animation to begin
+    var animation_mid_first_pos = $('#create').height();
+    var animation_mid_second_pos = animation_mid_first_pos + $('#features').height();
+    var animation_mid_third_pos = animation_mid_second_pos + $('#news').height();
+    var animation_mid_fourth_pos = animation_mid_third_pos + $('#pricing').height();
+    var animation_mid_fifth_pos = animation_mid_fourth_pos + $('#learn').height();
+    var animation_mid_sixth_pos = animation_mid_fifth_pos + $('#about').height();
+    var animation_end_pos = animation_mid_sixth_pos + $('#contact').height(); //where you want the animation to stop
+
+    //first  #C6C6C6  rgb(198,198,198)
+    //second  #215660  rgb(33,86,96)
+    //third  #FFF56C  rgb(255,245,108)
+    //fourth  #C6C6C6  rgb(198,198,198)
+    //fifth  #666666  rgb(102,102,102)
+    //sixth  #215660  rgb(33,86,96)
+    //seventh  #ffffff  rgb(255,255,255)
+
+    var beginning_color = new $.Color( 'rgb(198,198,198)' ); //we can set this here, but it'd probably be better to get it from the CSS; for the example we're setting it here.
+    var second_color = new $.Color( 'rgb(33,86,96)' );
+    var third_color = new $.Color( 'rgb(255,245,108)' );
+    var fourth_color = new $.Color( 'rgb(198,198,198)' );
+    var fifth_color = new $.Color( 'rgb(102,102,102)' );
+    var sixth_color = new $.Color( 'rgb(33,86,96)' );
+    var ending_color = new $.Color( 'rgb(255,255,255)' ); //what color we want to use in the end
+
+    $(document).scroll(function() {
+        scroll_pos = $(this).scrollTop();
+
+        if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) { 
+
+        	var percentScrolled = "";
+        	var newColor = "";
+        	var newRed = "";
+        	var newGreen = "";
+        	var newBlue = "";
+        	var bodyElement = $('body');
+
+            //we want to calculate the relevant transitional rgb value
+            if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_mid_first_pos) {
+	            percentScrolled = scroll_pos / ( animation_mid_first_pos - animation_begin_pos );
+	            newRed = beginning_color.red() + ( ( second_color.red() - beginning_color.red() ) * percentScrolled );
+	            newGreen = beginning_color.green() + ( ( second_color.green() - beginning_color.green() ) * percentScrolled );
+	            newBlue = beginning_color.blue() + ( ( second_color.blue() - beginning_color.blue() ) * percentScrolled );
+	            
+	            newColor = new $.Color( newRed, newGreen, newBlue );
+	            bodyElement.animate({ backgroundColor: newColor }, 0);
+            }
+            else if(scroll_pos > animation_mid_first_pos && scroll_pos <= animation_mid_second_pos) {
+	            percentScrolled = ( scroll_pos - animation_mid_first_pos ) / ( animation_mid_second_pos - animation_mid_first_pos );
+	            newRed = second_color.red() + ( ( third_color.red() - second_color.red() ) * percentScrolled );
+	            newGreen = second_color.green() + ( ( third_color.green() - second_color.green() ) * percentScrolled );
+	            newBlue = second_color.blue() + ( ( third_color.blue() - second_color.blue() ) * percentScrolled );
+	            
+	            newColor = new $.Color( newRed, newGreen, newBlue );
+	            bodyElement.animate({ backgroundColor: newColor }, 0);
+            }
+            else if(scroll_pos > animation_mid_second_pos && scroll_pos <= animation_mid_third_pos) {
+	            percentScrolled = ( scroll_pos - animation_mid_second_pos ) / ( animation_mid_third_pos - animation_mid_second_pos );
+	            newRed = third_color.red() + ( ( fourth_color.red() - third_color.red() ) * percentScrolled );
+	            newGreen = third_color.green() + ( ( fourth_color.green() - third_color.green() ) * percentScrolled );
+	            newBlue = third_color.blue() + ( ( fourth_color.blue() - third_color.blue() ) * percentScrolled );
+	            
+	            newColor = new $.Color( newRed, newGreen, newBlue );
+	            bodyElement.animate({ backgroundColor: newColor }, 0);
+            }
+            else if(scroll_pos > animation_mid_third_pos && scroll_pos <= animation_mid_fourth_pos) {
+	            percentScrolled = ( scroll_pos - animation_mid_third_pos ) / ( animation_mid_fourth_pos - animation_mid_third_pos );
+	            newRed = fourth_color.red() + ( ( fifth_color.red() - fourth_color.red() ) * percentScrolled );
+	            newGreen = fourth_color.green() + ( ( fifth_color.green() - fourth_color.green() ) * percentScrolled );
+	            newBlue = fourth_color.blue() + ( ( fifth_color.blue() - fourth_color.blue() ) * percentScrolled );
+	            
+	            newColor = new $.Color( newRed, newGreen, newBlue );
+	            bodyElement.animate({ backgroundColor: newColor }, 0);
+            }
+            else if(scroll_pos > animation_mid_fourth_pos && scroll_pos <= animation_mid_fifth_pos) {
+	            percentScrolled = ( scroll_pos - animation_mid_fourth_pos ) / ( animation_mid_fifth_pos - animation_mid_fourth_pos );
+	            newRed = fifth_color.red() + ( ( sixth_color.red() - fifth_color.red() ) * percentScrolled );
+	            newGreen = fifth_color.green() + ( ( sixth_color.green() - fifth_color.green() ) * percentScrolled );
+	            newBlue = fifth_color.blue() + ( ( sixth_color.blue() - fifth_color.blue() ) * percentScrolled );
+	            
+	            newColor = new $.Color( newRed, newGreen, newBlue );
+	            bodyElement.animate({ backgroundColor: newColor }, 0);
+            }
+             else if(scroll_pos > animation_mid_fifth_pos && scroll_pos <= animation_mid_sixth_pos) {
+	            percentScrolled = ( scroll_pos - animation_mid_fifth_pos ) / ( animation_mid_sixth_pos - animation_mid_fifth_pos );
+	            console.log(percentScrolled);
+	            newRed = sixth_color.red() + ( ( ending_color.red() - sixth_color.red() ) * percentScrolled );
+	            newGreen = sixth_color.green() + ( ( ending_color.green() - sixth_color.green() ) * percentScrolled );
+	            newBlue = sixth_color.blue() + ( ( ending_color.blue() - sixth_color.blue() ) * percentScrolled );
+	            
+	            newColor = new $.Color( newRed, newGreen, newBlue );
+	            bodyElement.animate({ backgroundColor: newColor }, 0);
+            }
+/*            else if(scroll_pos > animation_mid_second_pos && scroll_pos <= animation_mid_third_pos) {
+	            percentScrolled = ( scroll_pos - animation_mid_second_pos ) / ( animation_mid_third_pos - animation_mid_second_pos );
+	            console.log(percentScrolled);
+	            newRed = third_color.red() + ( ( fourth_color.red() - third_color.red() ) * percentScrolled );
+	            newGreen = third_color.green() + ( ( fourth_color.green() - third_color.green() ) * percentScrolled );
+	            newBlue = third_color.blue() + ( ( fourth_color.blue() - third_color.blue() ) * percentScrolled );
+	            
+	            newColor = new $.Color( newRed, newGreen, newBlue );
+	            $('body').animate({ backgroundColor: newColor }, 0);
+            }*/
+        }
+    });
+
 	// Cache the Window object
 	$window = $(window);
                 
