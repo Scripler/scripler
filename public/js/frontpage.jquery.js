0989bc12c1d70bc3b742c76644110ca949da8dc5
Gabriel <gabriel@scripler.com>
2014-02-24 15:46:59 +0100

    changed email format, added gabriel, converted spaces to tabs

--- public/js/frontpage.jquery.js
+++ public/js/frontpage.jquery.js
@@ -14,19 +14,19 @@ $(document).ready(function(){
 		windowSize();
 	}
 
-    function isScrolledIntoView(elem) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
+	function isScrolledIntoView(elem) {
+		var docViewTop = $(window).scrollTop();
+		var docViewBottom = docViewTop + $(window).height();
 
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(elem).height();
+		var elemTop = $(elem).offset().top;
+		var elemBottom = elemTop + $(elem).height();
 
-        return ( (elemBottom >= docViewTop) && (elemTop <= docViewBottom)
-            && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
-    }
+		return ( (elemBottom >= docViewTop) && (elemTop <= docViewBottom)
+			&& (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
+	}
 
-    $(".menu-top li.navpoint").on("click", function(event){
-    	event.preventDefault();
+	$(".menu-top li.navpoint").on("click", function(event){
+		event.preventDefault();
 
 		if ($(this).attr("data") == "create") {
 			document.location.href = "/create";
@@ -37,29 +37,29 @@ $(document).ready(function(){
 			return;
 		}
 
-    	var gotoPoint = "#" + $(this).attr("data");
+		var gotoPoint = "#" + $(this).attr("data");
 
-        if ( !isScrolledIntoView($(gotoPoint)) ) {
-            $('html, body').stop(true,true).animate({scrollTop: $(gotoPoint).offset().top}, 800);
-        }
+		if ( !isScrolledIntoView($(gotoPoint)) ) {
+			$('html, body').stop(true,true).animate({scrollTop: $(gotoPoint).offset().top}, 800);
+		}
 
 		/*bodyElement.animate({
 			scrollTop: $(gotoPoint).offset().top
 		}, {
 			duration: 2000,
 			step: function( now, fx ){
-        		documentScrollColor();
+				documentScrollColor();
 			}
 		});*/
 	});
 
 	$( "#login-form" ).submit(function( event ) {
-  		event.preventDefault();
+		event.preventDefault();
 		$.ajax({
 			url: '/user/login',
-            type: 'POST',
-            data: "email=" + $("#login-email").val() + "&password=" + $("#login-password").val(),
-            dataType: 'json',
+			type: 'POST',
+			data: "email=" + $("#login-email").val() + "&password=" + $("#login-password").val(),
+			dataType: 'json',
 			success: function( data ) {
 				document.location.href = "/create";
 			}
@@ -67,77 +67,59 @@ $(document).ready(function(){
 	});
 
 	$( ".menu-login" ).one("click", function( event ) {
-        $( "#login" ).animate({ "paddingBottom": "20", "paddingTop": "70" }, 800);
+		$( "#login" ).animate({ "paddingBottom": "20", "paddingTop": "70" }, 800);
 	})
 
-    $( ".menu-login" ).on("click", function( event ) {
-        $(":animated").promise().done( function() {
-            $( "#login-email" ).focus();
-        });
-    })
+	$( ".menu-login" ).on("click", function( event ) {
+		$(":animated").promise().done( function() {
+			$( "#login-email" ).focus();
+		});
+	})
 
-    $(".to-top").on("click", function(event){
-    	event.preventDefault();
-    	var gotoPoint = "#scripler";
+	$(".to-top").on("click", function(event){
+		event.preventDefault();
+		var gotoPoint = "#scripler";
 
-    	$('html, body').animate({scrollTop: $(gotoPoint).offset().top}, 800);
+		$('html, body').animate({scrollTop: $(gotoPoint).offset().top}, 800);
 	});
 
-    $("#about").on('click', '.seeteam, .prev', function(e) {
-    	e.preventDefault();
+	function GetURLParameter(sParam) {
+		//var sPageURL = window.location.search.substring(1);
+		var sPageURL = window.location.hash;
+		var sURLVariables = sPageURL.split('?');
 
-		var target = $(this).attr("class");
-        if (target == 'seeteam') {
-        	$("#about article").animate({
-            	left: '-=100%'
-        	}, 'slow');
-        	$('#about .prev').fadeIn('slow');
-        }
-        else {
-        	$("#about article").animate({
-            	left: '+=100%'
-        	}, 'slow');
-        	$('#about .prev').fadeOut('slow');
-        }
-    });
+		for (var i = 0; i < sURLVariables.length; i++) {
+			var sParameterName = sURLVariables[i].split('=');
 
-    function GetURLParameter(sParam) {
-        //var sPageURL = window.location.search.substring(1);
-        var sPageURL = window.location.hash;
-        var sURLVariables = sPageURL.split('?');
+			if (sParameterName[0] == sParam) {
+				return sParameterName[1];
+			}
+		}
+	}
 
-        for (var i = 0; i < sURLVariables.length; i++) {
-            var sParameterName = sURLVariables[i].split('=');
+	var CenterLatlng = new google.maps.LatLng(55.6654175,12.5815801);
+	var ScriplerLatlng = new google.maps.LatLng(55.669104,12.611121);
+	var mapOptions = {
+		center: CenterLatlng,
+		zoom: 14,
+		minZoom: 10,
+		maxZoom: 16,
+		disableDefaultUI: true,
+		scrollwheel: false,
+		navigationControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		draggable: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"),
+		mapOptions);
 
-            if (sParameterName[0] == sParam) {
-                return sParameterName[1];
-            }
-        }
-    }
-
-    var CenterLatlng = new google.maps.LatLng(55.6654175,12.5815801);
-    var ScriplerLatlng = new google.maps.LatLng(55.669104,12.611121);
-    var mapOptions = {
-        center: CenterLatlng,
-        zoom: 14,
-        minZoom: 10,
-        maxZoom: 16,
-        disableDefaultUI: true,
-	    scrollwheel: false,
-	    navigationControl: false,
-	    mapTypeControl: false,
-	    scaleControl: false,
-	    draggable: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: ScriplerLatlng,
-        map: map,
-        title: 'Scripler'
-    });
+	var marker = new google.maps.Marker({
+		position: ScriplerLatlng,
+		map: map,
+		title: 'Scripler'
+	});
 });
 
 /* Create HTML5 elements for IE */
