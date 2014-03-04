70955beaccdc2a02b7295191c3846ce26777123f
Gabriel <gabriel@scripler.com>
2014-03-04 15:46:44 +0100

    handling demo mode, when registered save all demo projects

--- public/create/scripts/create_controller.js
+++ public/create/scripts/create_controller.js
@@ -31,22 +31,27 @@ function PublicationsCtrl ( $scope, $http, userService ) {
 
 	$scope.$on('user:updated', function( event, user ) {
 		$scope.user = user;
-		//iterate through publications and call add
+
+		if ( user._id ) {
+			angular.forEach($scope.publications, function( publication, index ) {
+				$http.post('/project', angular.toJson( publication ) )
+					.success( function( data ) {
+						$scope.publications[index] = data.project;
+				});
+			})
+			$scope.getList();
+		}
 	});
 
-	$scope.publications = [
-		{ id:'00001', name:'Demo Title 1', created:'1363359600', changed:'1365606000' }
-	];
-
-	if ( $scope.user._id ) {
+	$scope.getList = function() {
 		$http.get('/project/list')
 			.success( function( data ) {
 				$scope.publications = [];
 				angular.forEach(data.projects, function( project ) {
-					$scope.publications.push( {id: project._id, name: project.name, created: project.created, changed: project.modified} );
+					$scope.publications.push( project );
 				})
 		});
-	}
+	};
 
 	$scope.toggleElement = function( element ) {
 		if ($scope.element != true && $scope.element != false) {
@@ -58,38 +63,51 @@ function PublicationsCtrl ( $scope, $http, userService ) {
 	};
 
 	$scope.addPublication = function() {
-		var index = $scope.publications.length + 1;
-		var name = "Title " + index;
-		var data = {};
-		data.name = name;
+		var order = $scope.publications.length;
+		var name = "Title " + order;
+		var publication = {};
+		publication.name = name;
+		publication.order = order;
 
-		$http.post('/project', angular.toJson( data ) )
-			.success( function( data ) {
-				var project = data.project;
-				$scope.publications.push( {id: project._id, name: project.name, created: project.created, changed: project.modified} );
-			});
+		if ( $scope.user._id ) {
+			$http.post('/project', angular.toJson( publication ) )
+				.success( function( data ) {
+					$scope.publications.push( data.project );
+				});
+		} else {
+			publication._id = Date.now();
+			$scope.publications.push( publication );
+			//save to localStorage
+		}
 	};
 
 	$scope.archivePublication = function( publication ) {
-		$http.put('/project/' + publication.id + '/archive')
-			.success( function() {
-				$scope.publications.splice($scope.publications.indexOf(publication), 1);
-			});
+		if ( $scope.user._id ) {
+			$http.put('/project/' + publication._id + '/archive')
+				.success( function() {});
+		}
+		$scope.publications.splice($scope.publications.indexOf( publication ), 1);
 	};
 
 	$scope.renamePublication = function( publication ) {
-		var data = {};
-		data.name = publication.name;
-		$http.put('/project/' + publication.id + '/rename', angular.toJson( data ) )
-			.success( function() {});
+		if ( $scope.user._id ) {
+			$http.put('/project/' + publication._id + '/rename', angular.toJson( publication ) )
+				.success( function() {});
+		}
 	};
 
 	$scope.copyPublication = function( publication ) {
-		$http.post('/project/' + publication.id + '/copy')
-			.success( function( data ) {
-				var project = data.project;
-				$scope.publications.push( {id: project._id, name: project.name, created: project.created, changed: project.modified} );
-			});
+		//backend needs to support order for copying, because otherwise order is 0
+		if ( $scope.user._id ) {
+			$http.post('/project/' + publication._id + '/copy')
+				.success( function( data ) {
+					$scope.publications.push( data.project );
+				});
+		} else {
+			var publication = publication;
+			this.publication.name = publication.name + ' - Copy';
+			$scope.publications.push( this.publication );
+		}
 	};
 
 };
