1182e613f8b5b5ccac95c8a07d31df32d1a02488
Gabriel <gabriel@scripler.com>
2014-03-17 15:42:49 +0100

    added document on the side

--- public/create/scripts/project_controller.js
+++ public/create/scripts/project_controller.js
@@ -5,14 +5,14 @@ function projectController( $scope, $location, userService, projectsService ) {
 	// Scope, Project
 	$scope.chapters = [
         //ADD/FIX: Get Publications API Call, on success do change
-        {chapterNumber:'00001',chapterTitle:'Titel 1',chapterContent:'<h1>this is a test</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'bookbw'},
-        {chapterNumber:'00002',chapterTitle:'Titel 2',chapterContent:'<h1>this is a test 2</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'bookbw'},
-        {chapterNumber:'00003',chapterTitle:'Titel 3',chapterContent:'<h1>this is a test 3</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'futurebw'},
-        {chapterNumber:'00004',chapterTitle:'Titel 4',chapterContent:'<h1>this is a test 4</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'futurebw'},
-        {chapterNumber:'00005',chapterTitle:'Titel 5',chapterContent:'<h1>this is a test 5</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'pleasantbw'},
-        {chapterNumber:'00006',chapterTitle:'Titel 6',chapterContent:'<h1>this is a test 6</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'pleasantbw'},
-        {chapterNumber:'00007',chapterTitle:'Titel 7',chapterContent:'<h1>this is a test 7</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'bookbw'},
-        {chapterNumber:'00008',chapterTitle:'Titel 8',chapterContent:'<h1>this is a test 8</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'bookbw'}
+        {chapterNumber:'00001',title:'Document 1',chapterContent:'<h1>this is a test</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'bookbw'},
+        {chapterNumber:'00002',title:'Document 2',chapterContent:'<h1>this is a test 2</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'bookbw'},
+        {chapterNumber:'00003',title:'Document 3',chapterContent:'<h1>this is a test 3</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'futurebw'},
+        {chapterNumber:'00004',title:'Document 4',chapterContent:'<h1>this is a test 4</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'futurebw'},
+        {chapterNumber:'00005',title:'Document 5',chapterContent:'<h1>this is a test 5</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'pleasantbw'},
+        {chapterNumber:'00006',title:'Document 6',chapterContent:'<h1>this is a test 6</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'pleasantbw'},
+        {chapterNumber:'00007',title:'Document 7',chapterContent:'<h1>this is a test 7</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'bookbw'},
+        {chapterNumber:'00008',title:'Document 8',chapterContent:'<h1>this is a test 8</h1><p>First line of text</p><h2>this is a test</h2><p>Second line of text</p><h3>this is a test</h3><p>Third line of text</p>',chapterStyleSheet:'bookbw'}
     ];
 
 	$scope.$on('user:updated', function( event, user ) {
@@ -22,7 +22,15 @@ function projectController( $scope, $location, userService, projectsService ) {
 	});
 
 	$scope.addDocument = function() {
+		var order = $scope.chapters.length;
+		var title = "Document " + order;
+		var document = {};
+		document.title = title;
+		document.order = order;
 
+		$scope.chapters.push( document );
+
+		//$http.post('/document')
 	}
 
     function initiateMenus() {
