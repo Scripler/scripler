5567eb4f14f0aa480b889e0cb3e9258b7ce67f5e
Gabriel <gabriel@scripler.com>
2014-03-31 16:42:30 +0200

    added showArchivedDocuments to user

--- models/user.js
+++ models/user.js
@@ -20,6 +20,7 @@ var UserSchema = new Schema({
 	],
 	modified: { type: Date, default: Date.now },
 	showArchived: { type: Boolean, default: false },
+	showArchivedDocuments: { type: Boolean, default: false },
 	newsletter: { type: Boolean, default: true }
 });
 
