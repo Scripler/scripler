7bbda19409356805b44f66ea88d54b854b22816c
Morten Garbøl Franck <morten.garboel.franck@gmail.com>
2014-05-21 14:53:53 +0200

    Fixes #158.

--- routes/styleset.js
+++ routes/styleset.js
@@ -81,6 +81,40 @@ exports.open = function (req, res) {
 	res.send({styleset: req.styleset});
 }
 
+var updateOriginalStyleset = exports.updateOriginalStyleset = function(newStyleset, next) {
+	// Populate the original styleset (was not loaded)
+	Styleset.find({"_id": {$in: [newStyleset._id, newStyleset.original]}}).populate({path: 'styles'}).exec(function (err, populatedStylesets) {
+		if (err) {
+			return next(err);
+		}
+
+		if (populatedStylesets.length==2) {
+			var populatedOriginalStyleset;
+			var populatedNewStyleset;
+
+			if (populatedStylesets[0]._id == newStyleset.original) {
+				populatedNewStyleset = populatedStylesets[0];
+				populatedOriginalStyleset  = populatedStylesets[1];
+			} else {
+				populatedOriginalStyleset = populatedStylesets[0];
+				populatedNewStyleset = populatedStylesets[1];
+			}
+
+			styleset_utils.updateOriginalStyleset(populatedOriginalStyleset, populatedNewStyleset, function (err) {
+				populatedOriginalStyleset.save(function (err, updatedOriginalStyleset) {
+					if (err) {
+						return next(err);
+					}
+
+					return next(null, updatedOriginalStyleset);
+				});
+			});
+		} else {
+			return next("ERROR: could not find both new and original styleset!");
+		}
+	});
+}
+
 /**
  *
  * Update a styleset, i.e. its name and styles.
@@ -96,42 +130,7 @@ exports.open = function (req, res) {
 exports.update = function (req, res, next) {
 	var styleset = req.styleset;
 
-	var updateOriginalStyleset = function(newStyleset, next) {
-		// Populate the original styleset (was not loaded)
-		Styleset.find({"_id": {$in: [newStyleset._id, newStyleset.original]}}).populate({path: 'styles'}).exec(function (err, populatedStylesets) {
-			if (err) {
-				return next(err);
-			}
-
-			if (populatedStylesets.length==2) {
-				var populatedOriginalStyleset;
-				var populatedNewStyleset;
-
-				if (populatedStylesets[0]._id == newStyleset.original) {
-					populatedNewStyleset = populatedStylesets[0];
-					populatedOriginalStyleset  = populatedStylesets[1];
-				} else {
-					populatedOriginalStyleset = populatedStylesets[0];
-					populatedNewStyleset = populatedStylesets[1];
-				}
-
-				styleset_utils.updateOriginalStyleset(populatedOriginalStyleset, populatedNewStyleset, function (err) {
-					populatedOriginalStyleset.save(function (err, updatedOriginalStyleset) {
-						if (err) {
-							return next(err);
-						}
-
-						return next(null, updatedOriginalStyleset);
-					});
-				});
-			} else {
-				return next("ERROR: could not find both new and original styleset!");
-			}
-		});
-	};
-
 	styleset.name = req.body.name;
-
 	// TODO: implement some sort of safety check ensuring that we only accept styles from the current styleset? See also TODO in models/Styleset.styles
 	styleset.styles = req.body.styles;
 
