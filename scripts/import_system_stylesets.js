cc86c25d34f8e9175eb6cb0285d57edb07e8769c
Morten Garbøl Franck <morten.garboel.franck@gmail.com>
2014-05-25 12:55:01 +0200

    Fixes #156. NB! Only reads a tester system styleset, simplecolor.css.
    Also, changed the tester system styleset:
    
    * .toc -> .toc-item
    * p.colophon -> .colophon
    
    Requested change of the other system stylesets in #164.

--- scripts/import_system_stylesets.js
+++ scripts/import_system_stylesets.js
@@ -7,53 +7,9 @@ var conf = require('config');
 var async = require('async');
 var Styleset = require('../models/styleset.js').Styleset;
 var Style = require('../models/style.js').Style;
+var cssNames = require(path.join(__dirname, '../lib/css-names.json'));
 
-function toInternalJSON(json) {
-	var internalJSON;
-	var ruleList = json['rulelist'];
-	//console.log(JSON.stringify(ruleList, null, '\t'));
-	internalJSON = ruleList;
-	return internalJSON;
-}
-
-function createStyle(json, styleset, callback) {
-	var internalJSONStyle = toInternalJSON(json);
-	console.log("internalJSONStyle: " + JSON.stringify(internalJSONStyle, null, '\t') + "\n");
-
-	/**
-	 * if name is ~@font-face, skip
-	 *
-	 * tag:
-	 *
-	 * class: .<name>
-	 *
-	 * @type {string}
-	 */
-
-	var name = 'style';
-	var clazz = 'clazz';
-	var tag = 'tag';
-
-	var style = new Style({
-		name: name,
-		class: clazz,
-		css: css,
-		tag: tag,
-		stylesetId: styleset._id,
-		isSystem: true
-	});
-
-	style.save(function (err, style) {
-		if (err) {
-			callback(err);
-		}
-
-		styleset.styles.addToSet(style);
-		callback(null, style);
-	});
-}
-
-function createStyleset(stylesheetName, jsonStyles, next) {
+function createStyleset(stylesheetName, jsonStyleset, next) {
 	var styleset = new Styleset({
 		name: stylesheetName,
 		isSystem: true
@@ -64,13 +20,60 @@ function createStyleset(stylesheetName, jsonStyles, next) {
 			return next(err);
 		}
 
+		// TODO: will the styles always be inside a 'rulelist'?
+		var jsonStyles = jsonStyleset['rulelist'];
+
 		async.each(jsonStyles, function (jsonStyle, callback) {
-			createStyle(jsonStyle, styleset, callback);
+			var type = jsonStyle['type'];
+			if (type) {
+				var name;
+				var clazz;
+				var tag;
+
+				if (type == 'fontface') {
+					console.log('Skipping "fontface" style...(should be added as non-editable CSS (that is only included if used))');
+					callback(null);
+				} else if (type == 'style') {
+					var selector = jsonStyle['selector'];
+					var isClass = selector.indexOf('.') == 0;
+
+					if (isClass) {
+						clazz = selector.slice(1);
+						name = cssNames[clazz];
+					} else {
+						tag = selector;
+						name = cssNames[tag];
+					}
+
+					var declarations = jsonStyle['declarations'];
+
+					var style = new Style({
+						name: name,
+						class: clazz,
+						css: declarations,
+						tag: tag,
+						stylesetId: styleset._id,
+						isSystem: true
+					});
+
+					style.save(function (err, style) {
+						if (err) {
+							callback(err);
+						}
+
+						styleset.styles.addToSet(style);
+						callback(null);
+					});
+				}
+			} else {
+				callback('"type" empty');
+			}
 		}, function (err) {
 			if (err) {
 				return next(err);
 			}
 
+			// The styles we just created were also added to the styleset so we must save the styleset again
 			styleset.save(function (err, styleset) {
 				if (err) {
 					return next(err);
@@ -83,6 +86,7 @@ function createStyleset(stylesheetName, jsonStyles, next) {
 	});
 }
 
+// TODO: read all stylesheets (.css files) in the system stylesets dir
 var stylesheetName = 'simplecolor';
 var cssFilename = path.join(__dirname, '../public/create/stylesets/' + stylesheetName + '.css');
 console.log(cssFilename);
@@ -98,7 +102,9 @@ mongoose.connect(conf.db.uri);
 createStyleset(stylesheetName, json, function (err, styleset) {
 	if (err) {
 		console.log(err);
+		process.exit(1);
 	}
 
 	console.log('Created and saved styleset ' + styleset);
+	process.exit(0);
 });
