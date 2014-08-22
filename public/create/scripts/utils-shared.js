8b21a78a48479f583ca70ecbafa0e2914e44b235
Gabriel <gabriel@scripler.com>
2014-08-22 11:52:54 +0200

    removed for now, since unused function

--- public/create/scripts/utils-shared.js
+++ public/create/scripts/utils-shared.js
@@ -59,31 +59,6 @@
 	}
 
 	/**
-	*
-	* Move items arround in an array with safety check for negative indexes
-	* old_index item that you want to move
-	* new_index where to move the item
-	* can be called as .move(0,1) on any array
-	*
-	**/
-	Array.prototype.move = function (old_index, new_index) {
-		while (old_index < 0) {
-			old_index += this.length;
-		}
-		while (new_index < 0) {
-			new_index += this.length;
-		}
-		if (new_index >= this.length) {
-			var k = new_index - this.length;
-			while ((k--) + 1) {
-				this.push(undefined);
-			}
-		}
-		this.splice(new_index, 0, this.splice(old_index, 1)[0]);
-		//return this;
-	};
-
-	/**
 	 * Check if two Mongoose objects are idential based on _id.
 	 *
 	 * @param obj1 Either ObjectId, _id string, or complete Mongoose object
