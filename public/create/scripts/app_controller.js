2cddbcdf5f38f1a3d79f9bc1750d4805104eb8db
allanmc <allanmc@gmail.com>
2014-08-30 15:40:29 +0200

    Removed style-selector from toolbar

--- public/create/scripts/app_controller.js
+++ public/create/scripts/app_controller.js
@@ -290,7 +290,7 @@ app.directive('ckEditor', function( $window, $rootScope, $timeout ) {
 				//Load css sheet via angualr here
 				toolbar: [
 					//['Source'], ['Undo'], ['Redo'], ['Paste'], ['PasteFromWord'], ['Styles'], ['Bold'], ['Italic'], ['Underline'], ['Strike'], ['JustifyLeft'], ['JustifyCenter'], ['JustifyRight'], ['JustifyBlock'], ['NumberedList'], ['BulletedList'], ['Image'], ['Link'], ['TextColor'], ['BGColor']
-					['Undo'], ['Redo'], ['Styles'], ['Bold'], ['Italic'], ['Underline'], ['Strike'], ['JustifyLeft'], ['JustifyCenter'], ['JustifyRight'], ['JustifyBlock'], ['NumberedList'], ['BulletedList'], ['Image'], ['Link'], ['Source'] // TODO: Remove "Source" button before "real" launch (ALPHA? BETA?)
+					['Undo'], ['Redo'], ['Bold'], ['Italic'], ['Underline'], ['Strike'], ['JustifyLeft'], ['JustifyCenter'], ['JustifyRight'], ['JustifyBlock'], ['NumberedList'], ['BulletedList'], ['Image'], ['Link'], ['Source'] // TODO: Remove "Source" button before "real" launch (ALPHA? BETA?)
 				],
 				removeButtons: 'language,CreateDiv,Flash,Iframe'
 			});
