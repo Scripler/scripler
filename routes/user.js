46c8d543bf93f98ca854b5fe8ca8ecdced1da932
allanmc <allanmc@gmail.com>
2014-10-15 22:01:50 +0200

    Added logging of verify-email urls

--- routes/user.js
+++ routes/user.js
@@ -324,10 +324,12 @@ exports.edit = function (req, res, next) {
 			}
 			user.email = email;
 			if ('test' != env) {
+				var url = conf.app.url_prefix + 'user/' + user._id + '/verify/' + hashEmail(user.email);
+				logger.info("Password verify url for " + user.email + ": " + url);
 				emailer.sendUserEmail(
 					user,
 					[
-						{name: "URL", content: conf.app.url_prefix + 'user/' + user._id + '/verify/' + hashEmail(user.email)}
+						{name: "URL", content: url}
 					],
 					'verify-email'
 				);
