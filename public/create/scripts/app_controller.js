562b8abe065d01a7b5a775d35871706a635567ee
Morten Garbøl Franck <morten.garboel.franck@gmail.com>
2014-11-22 11:40:50 +0100

    Fixes #682.

--- public/create/scripts/app_controller.js
+++ public/create/scripts/app_controller.js
@@ -9,6 +9,7 @@ app.controller('appController', [ '$http', '$scope', 'userService', '$rootScope'
 		$scope.errors.email = 'Email is invalid';
 		$scope.errors.password = 'Six characters minimum';
 		$scope.registrationText = 'Hey, stranger. Register to save your work!';
+		$scope.socialRegistrationText = 'Or register via:';
 		$scope.EMAIL_REGEXP = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 		$scope.user = {};
