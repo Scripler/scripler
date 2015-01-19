519c84f4b59d3919c2562c388fd3a0fb36fc35d0
allanmc <allanmc@gmail.com>
2015-01-19 21:35:25 +0100

    Use protocol relative urls for external resources, so that we can use https.

--- public/create/scripts/project_controller.js
+++ public/create/scripts/project_controller.js
@@ -1289,15 +1289,12 @@ function projectController( $scope, $location, userService, projectsService, $ht
 	}
 
 	function constructImageTag( image ) {
-		var imageTag = '<img src="http://' + $location.host() + '/project/' + $scope.pid + '/images/' + image.name + '" />';
-		//removed class="regularImage"
+		var imageTag = '<img src="//' + $location.host() + '/project/' + $scope.pid + '/images/' + image.name + '" />';
 		return imageTag;
 	}
 
-	//use the :3000 port to allow images to be uploaded on localhost
-
 	function constructCoverTag(image){
-		var imageTag = '<img class="cover" src="http://' + $location.host() + '/project/' + $scope.pid + '/images/' + image.name + '" />';
+		var imageTag = '<img class="cover" src="//' + $location.host() + '/project/' + $scope.pid + '/images/' + image.name + '" />';
 		return imageTag;
 	}
 
