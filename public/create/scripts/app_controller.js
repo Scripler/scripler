d848275cb3527da88241d9e5f59618efa4a4f79e
Ole Jorgensen <olj@trustpilot.com>
2015-02-05 23:42:53 +0100

    changed the directive for styleset, style and projects, removed the old directive

--- public/create/scripts/app_controller.js
+++ public/create/scripts/app_controller.js
@@ -299,19 +299,6 @@ app.directive('focusOn', function( $timeout, $parse ) {
 	};
 });
 
-app.directive('onEnter', function() {
-	return function( scope, element, attrs ) {
-		element.bind("keydown keypress", function( event ) {
-			if(event.which === 13) {
-				scope.$apply(function(){
-				scope.$eval(attrs.onEnter, {'event': event});
-			});
-				event.preventDefault();
-			}
-		});
-	};
-});
-
 app.directive('blurOnEnter', function() {
 	return function(scope, element, attributes) {
 		element.bind("keydown keypress", function( event ) {
