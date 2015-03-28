19ece3cce5a3de3e210407bbc4ab3fab5e919792
allanmc <allanmc@gmail.com>
2015-03-28 21:23:24 +0100

    Implemented review comments

--- scripts/fix-styleset-access.js
+++ scripts/fix-styleset-access.js
@@ -4,13 +4,13 @@ var conf = require('config');
 
 mongoose.connect(conf.db.uri);
 
-Styleset.update({"name": {"$nin": conf.free.stylesets} }, {"$set": {"accessLevels": ['premium', 'professional']}}, {multi: true}, function(err, numAffected) {
+Styleset.update({"name": {"$nin": conf.subscriptions.free.stylesets} }, {"$set": {"accessLevels": ['premium', 'professional']}}, {multi: true}, function(err, numAffected) {
 	if (err) {
 		cosole.log(err);
 		return process.exit(1);
 	}
 	console.log("Updated " + numAffected + " stylesets to premium.");
-	Styleset.update({"name": {"$in": conf.free.stylesets} }, {"$set": {"accessLevels": ['free', 'premium', 'professional']}}, {multi: true}, function(err, numAffected) {
+	Styleset.update({"name": {"$in": conf.subscriptions.free.stylesets} }, {"$set": {"accessLevels": ['free', 'premium', 'professional']}}, {multi: true}, function(err, numAffected) {
 		if (err) {
 			cosole.log(err);
 			return process.exit(1);
