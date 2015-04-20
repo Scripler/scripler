2da8de730fb584cbae00d89ffbb240c38f61256c
Roxana <roxana.larisa.dobre@gmail.com>
2015-04-20 19:43:47 +0200

    fixes ToC not getting called inside the anchors and hyperlinks subtabs

--- public/create/scripts/project_controller.js
+++ public/create/scripts/project_controller.js
@@ -1277,6 +1277,7 @@ function projectController( $scope, $location, userService, projectsService, $ht
 	// status in function is witch insert option that is clicked, preserve will be used to make sure it is not toggeling the open/close state, but will always have it open
 	$scope.insertOptionChosen = function(status, preserve) {
 		$scope.activeInsertOption = ((status != $scope.activeInsertOption || preserve) ? status : null);
+		$scope.getToc();
         $scope.focusEditor();
     }
 
