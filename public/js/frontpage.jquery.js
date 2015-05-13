a827fd1c8b22d1f1a451388c409134fb292ef881
Roxana <roxana.larisa.dobre@gmail.com>
2015-05-13 15:03:15 +0200

    hiding login box on page load; enabled toggle of the login box

--- public/js/frontpage.jquery.js
+++ public/js/frontpage.jquery.js
@@ -23,8 +23,18 @@ $(document).ready(function() {
         return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
     }
 
+    $( ".menu-login" ).click(function() {
+        if (!isScrolledIntoView($("#login"))) {
+                $('html, body').stop(true, true).animate({
+                    scrollTop: $("#login").offset().top
+                }, 800);
+                $("#login").slideToggle();
+            }
+        $("#login").slideToggle();
+    });
+
     $(".navbar-nav .navpoint a").on("click", function(event) {
-        if ($(this).attr("data") != "blog" && $(this).attr("data") != "talk") {
+        if ($(this).attr("data") != "blog" && $(this).attr("data") != "talk" && $(this).attr("data") != "login") {
             event.preventDefault();
             if ($(this).attr("data") == "create") {
                 document.location.href = "/create";
@@ -198,7 +208,6 @@ $(document).ready(function() {
 
         // Switch login-article from login-form to password-reset-form, and display immediately
         //$("#login").css("paddingBottom", 10);
-        $("#login").css("paddingTop", 70);
         $("#login-form").css("display", "none");
         $("#password-reset-form").css("display", "block");
 
@@ -252,8 +261,7 @@ $(document).ready(function() {
                     $("#login").css("display", "none");
                 } else if (user.isDemo) {
                     $("#login").animate({
-                        "paddingBottom": "10",
-                        "paddingTop": "60"
+                        
                     }, 800);
                     $(".menu-login").css("display", "inline");
                 }
@@ -261,8 +269,7 @@ $(document).ready(function() {
         },
         error: function(request, status, error) {
             $("#login").animate({
-                "paddingBottom": "10",
-                "paddingTop": "40"
+                
             }, 800);
         }
     });
