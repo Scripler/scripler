27cff1d48158f543497fd381a7b5b37f5649bdcc
allanmc <allanmc@gmail.com>
2015-05-14 22:10:38 +0200

    Implemented review comments.

--- routes/server.js
+++ routes/server.js
@@ -14,9 +14,9 @@ exports.status = function (req, res, next) {
 			return next(err);
 		}
 		if (!styleset) {
-			return next("Styleset not found!");
+			return next("Styleset 'light-color' not found!");
 		}
-		var epubjsReadme = path.join('.', 'public', 'reader', 'epubjs', 'README.md');
+		var epubjsReadme = path.join('public', 'reader', 'epubjs', 'README.md');
 
 		// Check that submodules have been loaded (check epujs submodule)
 		fs.readFile(epubjsReadme, function (err, data) {
@@ -24,10 +24,10 @@ exports.status = function (req, res, next) {
 				return next(err);
 			}
 			if (!data) {
-				return next("EpubJS README.md could not be found. Has submodules been loaded?");
+				return next("EpubJS README.md could not be found. Have submodules been loaded?");
 			}
 			if (data.length <= 0) {
-				return next("EpubJS README.md looks empty. Has submodules been loaded?");
+				return next("EpubJS README.md looks empty. Have submodules been loaded?");
 			}
 
 			// Check that important processes are running (Production only)
@@ -40,7 +40,7 @@ exports.status = function (req, res, next) {
 					if (err) {
 						return next(err);
 					}
-					if (resultList.length == 2) {
+					if (resultList.length == 3) {
 						return res.send("OK");
 					} else {
 						return next('Invalid prcoesslist: ' + JSON.stringify(resultList));
